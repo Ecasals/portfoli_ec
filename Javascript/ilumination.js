cf28bbef65e9ab385f2112ec998a811e785e8f65
Ecasals <casals.eduard@gmail.com>
2017-06-16 17:53:41 +0200

    Add files via upload

---
+++ Javascript/ilumination.js
@@ -0,0 +1,37 @@
+document.getElementById("in_user_name").onclick = function () {
+    this.style.backgroundColor = "rgba(245,245,245,0.8)";
+    document.getElementById("info_user").style.color = "black";
+    document.getElementById("menu_options_container").style.height = "90%";
+    setTimeout(delay2, 1000);
+}
+
+
+document.getElementById("in_close").onclick = function () {
+    document.getElementById("menu_options_container").style.height = "0%";
+    var elements = document.getElementsByClassName("info_title");
+    for (var i = 0; i < elements.length; i++) {
+        elements[i].style.display = "none";
+    }
+    var elements = document.getElementsByClassName("info_dec");
+    for (var i = 0; i < elements.length; i++) {
+        elements[i].style.fontSize = "0px";
+    }
+    setTimeout(delay, 1000);
+    
+}
+
+function delay() {
+    document.getElementById("in_user_name").style.backgroundColor = "rgba(245,245,245,0)";
+}
+
+function delay2() {
+    var elements = document.getElementsByClassName("info_title");
+    for (var i = 0; i < elements.length; i++) {
+        elements[i].style.display = "block";
+    }
+    var elements = document.getElementsByClassName("info_dec");
+    for (var i = 0; i < elements.length; i++) {
+        elements[i].style.fontSize = "25px";
+    }
+}
+
